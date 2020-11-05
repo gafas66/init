14c9a581f8044b687c3af417074d209fc4e9f552
ESK <ekofoed@gmail.com>
2020-11-05 02:11:10 +0000

    Updated to use use-package; needs debug

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs init file (non-existing by default)
-
-;; Make sure we have our local library setup
+;; Some general info
 
 (setq is-DL (if (file-exists-p "/projects/ASIC/ridge/") t nil))
 (setq is-me (if (or (string= "ekofoed" (getenv "USER"))
@@ -11,10 +10,82 @@
 	    (is-DL "/home/ekofoed")
 	    (t (getenv "HOME"))))
 
-(add-to-list 'load-path (concat HOME "/.emacs.d/lisp"))
-(add-to-list 'load-path (concat HOME "/.emacs.d/bookmark+"))
-
 (setq is-linux (if (string= system-type "gnu/linux") t nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Make sure we have our local library setup from MALPA etc
+
+(require 'package)
+(package-initialize)
+
+;(add-to-list 'package-archives '("melpa"     . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable"     . "https://stable.melpa.org/packages/") t)
+
+(add-to-list 'load-path (concat HOME "/.emacs.d/lisp")) ;My own packages
+(package-refresh-contents)
+(package-install 'use-package)
+;(require 'use-package-ensure)
+;(setq use-package-ensure t)
+
+;;; Can't get below to auto-work
+;;;(use-package auto-package-update
+;;;	     :config
+;;;	     (setq auto-package-update-delete-old-versions t)
+;;;	     (setq auto-package-update-hide-results t)
+;;;	     (auto-package-update-maybe))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Done setting up package and use-package
+;; .. now install desired packages
+
+;;; (use-package clojure-mode)	      ;Syntax highlight
+;;; (use-package cider)		      ;REPL in emacs
+;;; (use-package paredit)		      ;Electrical grouping of parens
+;;; (use-package rainbow-delimiters)      ;paranthesis in different colors
+;;; (use-package magit)		      ;Git details in emacs
+;;; (use-package color-theme-modern	      ;Covers goldenrod
+;;;   :config
+;;;   (load-theme 'goldenrod t t)
+;;;   (enable-theme 'goldenrod))
+;;; 					;(use-package which-key :config (which-key-mode)) ;FIXME
+;;; ;(package-install 'which-key)(which-key-mode)
+;;; 					;(use-package better-defaults) ;FIXME
+;;; ;(package-install 'better-defaults)
+;;; (use-package paredit)
+;;; 					;(use-package ace-jump-mode) ;FIXME
+;;; ;(package-install 'ace-jump-mode)
+;;; 					;(use-package powerline :config (powerline-default-theme)) ;FIXME
+;;; ;(package-install 'powerline) (powerline-default-theme)
+;;; (use-package yaml-mode)
+;;; 
+;;; ;; Local in my ./lisp dir
+;;; (use-package tabbar		      ;Tabulate similar file-types (font size?)
+;;;   :config
+;;;   (tabbar-mode))
+;;; (use-package my-auto-insert)          ;My auto-comments and headings
+;;; (use-package markerpen		      ;Allow changing selected text color
+;;;   :config
+;;;   (global-set-key (kbd "<f8>")  'markerpen1)
+;;;   (global-set-key (kbd "<f9>")  'markerpen4)
+;;;   (global-set-key (kbd "<f10>") 'markerpen9))
+;;; (use-package verilog-mode	      ;For verilog files
+;;;   :config
+;;;   (autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
+;;;   (add-to-list 'auto-mode-alist '("\\.[ds]?vh?\\'" . verilog-mode)))
+;;; (use-package rake-mode)	              ;
+;;; (use-package epa-file)		      ;easyPG interface for GnuGPG
+;;; (use-package csv-mode)		      ;CSV files
+;;; (use-package uniquify		      ;Built-in package, same file names looks diff.
+;;;   :config
+;;;   (setq uniquify-buffer-name-style 'reverse))
+;;; (use-package other-modes)		      ;PT etc
+;;; (use-package fic-mode		      ;NOTE/TODO etc in files
+;;;   :config
+;;;   (setq p-modes '(tcl-mode-hook ruby-mode-hook perl-mode-hook cperl-mode-hook emacs-lisp-mode-hook python-mode-hook))
+;;;   (mapcar (lambda (mode) (add-hook mode 'fic-mode)) p-modes))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Done installing packages
 
 ;; Get screen info if on X
 (if is-linux
@@ -29,165 +100,30 @@
     (setq width  1920)
     (setq height 1080)))
 
-(when is-me
-  (when (> emacs-major-version 25) ;; Works on 24 onwards
-    ;; Lets get packages set up
-    (require 'package) ;; You might already have this line
-    (add-to-list 'package-archives
-		 '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;;    (when (< emacs-major-version 24) ;; Never to be used in this setting
-;;      ;; For important compatibility libraries like cl-lib
-;;      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-    
-    ;(add-to-list 'package-archives
-;		 '("marmalade" . "https://marmalade-repo.org/packages/") t)
-    (package-initialize) ;; You might already have this line
-    
-    (when (not package-archive-contents)
-      (package-refresh-contents))
-    (defvar my-packages '(
-			  clojure-mode
-			  paredit
-			  cider
-			  ;;groovy-mode ;;Mode used behind jenkins
-			  ;;smex ;;expand M-x fucntionality
-			  ;;projectile
-			  ;;rainbow-delimiters ;; Each paranthesis pair a different color, M-x rainbow-delimers-mode - or hook
-			  magit ;; Git stuff
-			  color-theme-modern
-			  ))
-    (dolist (p my-packages)
-      (when (not (package-installed-p p))
-	(package-install p)))))
+(cond ((= height 2160) (set-face-attribute 'default nil :height 140))
+      ((= height 1080) (set-face-attribute 'default nil :height 100))
+      (t nil))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
-(add-to-list 'auto-mode-alist '("\\.[ds]?vh?\\'" . verilog-mode))
-(load (concat HOME "/.emacs.d/lisp/markerpen.el"))
 
-;;; KB macros
+;; Various setup
 
-;;; Magit short-cut
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;(setq select-active-regions nil) ; KLUDGE waiting for propery-notify
-;; ORG mode
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
 (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "DONE")))
 
-(require 'epa-file) ;easyPG interface for GnuGPG
-(require 'csv-mode)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'reverse)
-
-;;; Setup decent colours
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-goldenrod)
-
-;; Other nice stuff
-(require 'other-modes)
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-(require 'tabbar)			;This gives tabs to switch between buffers in this windows
-(tabbar-mode)				;Activate now
-
-(require 'my-auto-insert)		;Insert header and footers in various files
-
-(autoload 'folding-mode "folding" "Folding mode" t) ;mode to hide/show parts of text
-(global-auto-revert-mode t)			    ;Reverts files to latest after 5 seconds if changed on disk
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;# NOTE Higlight NOTE FIXME KLUDGE words in buffer
-(require 'fic-mode)
-(setq p-modes '(tcl-mode-hook ruby-mode-hook perl-mode-hook cperl-mode-hook emacs-lisp-mode-hook python-mode-hook))
-(mapcar (lambda (mode) (add-hook mode 'fic-mode)) p-modes)
-
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (outline-minor-mode)
-            (setq outline-regexp " *\\(def \\|class\\|module\\)")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;; Highlight changes in file
-;;;(require 'hilit-chg)
-;;;(make-empty-face 'highlight-changes-saved-face)
-;;;(setq highlight-changes-face-list '(highlight-changes-saved-face))
-;;;
-;;;(defun DE-highlight-changes-rotate-faces ()
-;;;  (let ((toggle (eq highlight-changes-mode 'passive)))
-;;;    (when toggle (highlight-changes-mode t))
-;;;    (highlight-changes-rotate-faces)
-;;;    (when toggle (highlight-changes-mode nil))))
-;;;					; Example: activate highlight changes with rotating faces for C programming
-;;;(mapcar (lambda (mode)
-;;;	  (add-hook mode
-;;;		    (function (lambda ()
-;;;				(add-hook 'local-write-file-hooks 'DE-highlight-changes-rotate-faces)
-;;;				(highlight-changes-mode t)
-;;;				(highlight-changes-mode t)))))
-;;;	p-modes)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'rake-mode)
-
 (winner-mode 1)				;Allows revert windows content/position history w/ C-c <|> 
-
-;;; PAREDIT
-
-(require 'paredit)
-(autoload 'enable-paredit-mode "paredit"
-  "Turn on pseudo-structural editing of Lisp code."
-  t)
-
-;;; Start Paredit Mode on the fly with `M-x enable-paredit-mode RET',
-;;; or always enable it in a major mode `M' (e.g., `lisp') with:
-
-(add-hook 'lisp-mode-hook 'enable-paredit-mode)
-
-;;; Customize paredit using `eval-after-load':
-
-(eval-after-load 'paredit
-  '(progn
-     (define-key paredit-mode-map (kbd "ESC M-A-C-s-)")
-       'paredit-dwim)))
-
-
-					; Test out ido mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-create-new-buffer 'always)
-;(setq ido-file-extensions-order '(".tcl" ".pl"))
-(ido-mode 1)
-
 (ffap-bindings)				;ffap = fINDfILEaTPoint
-
-;; Stop stupid bell
 (setq visible-bell t)
 
-;; Might be needed
+;; Emacs shell setup
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(add-hook 'shell-mode-hook (lambda () (face-remap-set-base 'comint-highlight-prompt :inherit nil)))
 
-;;; NOTE  F1 is shell instead of "Help"
 (global-set-key [f1] 'shell)
 (setq display-buffer-alist '(("\\`\\*e?shell" display-buffer-same-window)))
 (global-set-key [f2] 'rename-buffer)
 
-; NOTE Outline mode magic TAB
-(eval-after-load 'outline
-  '(progn
-     (require 'outline-magic)
-     (define-key outline-minor-mode-map (kbd "<C-tab>") 'outline-cycle)))
-
- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-					; Setup own function keys
+				      ; Setup own function keys
 (defun truncate-lines-toggle ()
   (interactive)
   (if (equal truncate-lines t)
@@ -195,22 +131,12 @@
     (setq truncate-lines t))
   (redraw-display))
 (global-set-key (kbd "C-t") 'truncate-lines-toggle)
-
-(defun my-dirs ()
-  (interactive)
-  (dirs) (redraw-display)
-  (previous-line) (move-beginning-of-line nil) (kill-line)
-  (end-of-buffer)
-  (insert "cd ")
-  (insert (yank)))
 					; My own function keys
 (global-set-key (kbd "C-'") 'erase-buffer)
 (global-set-key (kbd "C-0") 'find-file-at-point)
 (global-set-key (kbd "C-x r p") 'replace-rectangle)
-(global-set-key (kbd "M-<return>") 'my-dirs)
 (global-set-key (kbd "M-g") 'goto-line)
 (global-set-key (kbd "C-x p") 'previous-multiframe-window)
-
 					; Use cperl-mode instead of the default perl-mode
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
@@ -219,12 +145,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
-(setq revert-without-query (quote (".*")))
-
 ;; Various useful settings
 (global-hi-lock-mode 1)
 (setq hi-lock-file-patterns-policy (lambda (pattern) t))
-(tool-bar-mode nil)
+(tool-bar-mode -1)
 (show-paren-mode t)
 (put 'erase-buffer 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
@@ -233,19 +157,6 @@
 (setq ansi-color-names-vector
       ["black" "tomato" "PaleGreen2" "gold1"
        "DeepSkyBlue1" "MediumOrchid1" "cyan" "white"])
-;(setq ansi-color-map (ansi-color-make-color-map))      # this helped
-;; (setq ansi-color-names-vector ; better contrast colors
-;;       ["black" "red4" "green4" "yellow4"
-;;        "blue3" "magenta4" "cyan4" "white"])
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-					; Other stuff
-;; FIXME  make completion buffers disappear after 3 seconds.
-;; FIXME (add-hook 'completion-setup-hook
-;; FIXME   (lambda () (run-at-time 3 nil
-;; FIXME     (lambda () (kill-buffer "*Completions*")))))
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun ek-hi-set ()
   (interactive)
@@ -265,31 +176,7 @@
 (global-set-key (kbd "<f6>") 'highlight-changes-mode)
 (global-set-key (kbd "<f7>") 'whitespace-mode)
 
-(global-set-key (kbd "<f8>")  'markerpen1)
-(global-set-key (kbd "<f9>")  'markerpen4)
-(global-set-key (kbd "<f10>") 'markerpen9)
-					; Start maximized
-;; (defun toggle-fullscreen (&optional f)
-;;   (interactive)
-;;   (let ((current-value (frame-parameter nil 'fullscreen)))
-;;     (set-frame-parameter nil 'fullscreen
-;;  			 (if (equal 'fullboth current-value)
-;;  			     (if (boundp 'old-fullscreen) old-fullscreen nil)
-;;  			   (progn (setq old-fullscreen current-value)
-;;  				  'fullboth)))))
-;; 
-;; (global-set-key [f11] 'toggle-fullscreen)
-;; (add-hook 'after-make-frame-functions 'toggle-fullscreen)
-
 (setq undo-outer-limit 1200000000)
-
-(defun ek-set-faces (HEIGHT)
-  (custom-set-faces
-   `(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil
-			   :strike-through nil :overline nil :underline nil
-			   :slant normal :weight normal
-			   :height ,HEIGHT :width normal
-			   :foundry "bitstream" :family "Courier"))))))
 
 (server-start) ;; For emacs to listen
 
@@ -300,7 +187,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(color-theme-modern magit paredit)))
+ '(package-selected-packages
+   '(powerline ace-jump-mode better-defaults which-key use-package rainbow-delimiters paredit magit color-theme-modern cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
